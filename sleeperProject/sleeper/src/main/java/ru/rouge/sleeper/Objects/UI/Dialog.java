9351b48719268b52c289a76ede839b887c0d0c6f
Evgenij <evgenijsavchik@gmail.com>
2013-11-13 00:42:22 +0800

    Dialog first step

--- sleeperProject/sleeper/src/main/java/ru/rouge/sleeper/Objects/UI/Dialog.java
+++ sleeperProject/sleeper/src/main/java/ru/rouge/sleeper/Objects/UI/Dialog.java
@@ -1,10 +1,21 @@
 package ru.rouge.sleeper.Objects.UI;
 
+import org.andengine.entity.scene.CameraScene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.AutoWrap;
+import org.andengine.entity.text.Text;
+import org.andengine.entity.text.TextOptions;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.HorizontalAlign;
+
+import ru.rouge.sleeper.Managers.ResourceManager;
+import ru.rouge.sleeper.WorldContext;
+
 /**
  * Created by Evgenij on 08.11.13.
  * Диалог в игре
  */
-public class Dialog
+public class Dialog extends CameraScene
 {
     //-----------------------------
     //Constants
@@ -14,17 +25,40 @@ public class Dialog
     //Variables
     //-----------------------------
 
+    private Text mTextMess;
+    private String mMesage;
+
     //-----------------------------
     //Ctors
     //-----------------------------
+
+    public Dialog(float pX, float pY, float pWidth, float pHeight, VertexBufferObjectManager pVertexBufferObjectManager)
+    {
+        super(WorldContext.getInstance().getCamera());
+        this.setPosition(pX, pY);
+        setBackgroundEnabled(false);
+
+        createDialog(0, 0, pWidth, pHeight, pVertexBufferObjectManager);
+    }
 
     //-----------------------------
     //Methods
     //-----------------------------
 
+    private void createDialog(float pX, float pY, float pWidth, float pHeight, VertexBufferObjectManager pVertexBufferObjectManager)
+    {
+        attachChild(new Sprite(pX, pY, pWidth, pHeight, ResourceManager.getInstance().mDialogBackground, pVertexBufferObjectManager));
+    }
+
     //-----------------------------
     //Getters/Setters
     //-----------------------------
+
+    public void setTextDialog(String mess)
+    {
+        this.mMesage = mess;
+        attachChild(new Text(50, 50, ResourceManager.getInstance().mGameFont, mess, 1000, new TextOptions(AutoWrap.LETTERS, (float)450, HorizontalAlign.CENTER, Text.LEADING_DEFAULT), ResourceManager.getInstance().mVBO));
+    }
 
     //-----------------------------
     //Inner Classes
