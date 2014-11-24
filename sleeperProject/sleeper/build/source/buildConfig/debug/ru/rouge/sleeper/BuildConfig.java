3460d9275ac5787f34ac244989fed3b202c77cfb
Evgenij <evgenijsavchik@gmail.com>
2014-11-24 19:13:49 +0800

    new type

--- sleeperProject/sleeper/build/source/buildConfig/debug/ru/rouge/sleeper/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ru.rouge.sleeper;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
